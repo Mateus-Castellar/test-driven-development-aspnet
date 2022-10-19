ceb534af2727ed24289a87d50cdf08bf1f7ec9b8
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-19 04:24:16 -0300

    feat: Command para adicionar item ao pedido

---
+++ tests/Store.Vendas.Application.Tests/Pedidos/AdicionarItemPedidoCommandTests.cs
@@ -0,0 +1,42 @@
+﻿using Store.Vendas.Application.Commands;
+using Xunit;
+namespace Store.Vendas.Application.Tests.Pedidos
+{
+    public class AdicionarItemPedidoCommandTests
+    {
+        [Fact(DisplayName = "Adicionar Item Command Válido")]
+        [Trait("Categoria", "Vendas - Pedido Command")]
+        public void AdicionarItemPedidoCommand_CommandEstaValido_DevePassarNaValidacao()
+        {
+            // Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.NewGuid(),
+                Guid.NewGuid(), "Produto teste", 2, 100);
+
+            // Act
+            var result = pedidoCommand.EhValido();
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact(DisplayName = "Adicionar Item Command Inválido")]
+        [Trait("Categoria", "Vendas - Pedido Command")]
+        public void AdicionarItemPedidoCommand_CommandEstaInvalido_NaoDevePassarNaValidacao()
+        {
+            // Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.Empty,
+                Guid.Empty, "", 0, 0);
+
+            // Act
+            var result = pedidoCommand.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.Contains(AdicionarItemPedidoValidation.IdClienteErroMsg, pedidoCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidation.IdProdutoErroMsg, pedidoCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidation.NomeErroMsg, pedidoCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidation.QtdMinErroMsg, pedidoCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidation.ValorErroMsg, pedidoCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+        }
+    }
+}
